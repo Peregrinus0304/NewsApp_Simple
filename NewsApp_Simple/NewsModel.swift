3d3a8a4fa0d0aa5b2c936cc9fea476ccc07bb1a7
Perefrinus <motruk007@gmail.com>
2020-11-24 22:29:50 +0100

    initial commit

---
+++ NewsApp_Simple/NewsModel.swift
@@ -0,0 +1,80 @@
+//
+//  NewsModel.swift
+//  NewsApp_Simple
+//
+//  Created by Ozzy on 24.11.2020.
+//  Copyright © 2020 Taras Motruk. All rights reserved.
+//
+
+import Foundation
+
+class News: Codable {
+
+  
+    
+    
+    
+    //MARK: - Struct
+    
+    private struct Result: Codable {
+        var status: String
+        var totalResults: Int
+        var articles: [Articles]
+    }
+    
+    private struct Articles: Codable {
+        var source: Source
+        var author: String?
+        var title: String
+        var description: String
+        var url: String
+        var urlToImage: String
+        var publishedAt: String
+        var content: String
+    }
+    
+    private struct Source: Codable {
+        var id: String?
+        var name: String
+    }
+    
+  var country: String
+  var category: String
+   private var result: Result
+    
+    
+    //MARK: - Reference
+    
+    
+    let apiKey = "9d22dc6191124789b5721d6f482ec503"
+    //MARK: - Parsing
+    
+    func getData(completed: @escaping () -> ()){
+       
+        // Create URL
+        let urlString = "http://newsapi.org/v2/top-headlines?country=\(self.country)&category=\(self.category)&apiKey=9d22dc6191124789b5721d6f482ec503"
+        guard let url = URL(string: urlString) else {
+            print("ERROR: incorrect URL \(urlString)")
+            return
+        }
+    
+        let session = URLSession.shared
+        
+        // Get data with data task
+        
+        let dataTask = session.dataTask(with: url) { (data, responce, error) in
+            if let error = error{
+                print("ERROR!\(error.localizedDescription)")
+            }
+            do {
+                 let result = try JSONDecoder().decode(Result.self, from: data!)
+                self.result = result
+                
+            } catch {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            completed()
+        }
+        dataTask.resume()
+    }
+}
---
+++ NewsApp_Simple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp_Simple
+//
+//  Created by Ozzy on 23.11.2020.
+//  Copyright © 2020 Taras Motruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
