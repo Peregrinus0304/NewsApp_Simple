18a5e8ea0ea1aae6184dbc0279e25a6511b57fbe
Perefrinus <motruk007@gmail.com>
2020-12-08 23:23:41 +0100

    add UIRefreshControl & minor UI changes

--- NewsApp_Simple/ViewController.swift
+++ NewsApp_Simple/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     var zeroRectTextField = UITextField()
     var picker = UIPickerView()
     var toolbar = UIToolbar()
+    private let refreshControl = UIRefreshControl()
     var country = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"]
     var category = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
     var selectedCategory = "business"
@@ -30,73 +31,88 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        // getting data from the model
         news.getData{
             DispatchQueue.main.async {
                 self.newsCollectionView.reloadData()
             }
         }
         
+        // MARK: - Initialize UI elements
+        
         navigationBar.topItem!.title = "\(news.country) - \(news.category)"
         
+        // Add Refresh Control to the CollectionView
+        if #available(iOS 10.0, *) {
+            newsCollectionView.refreshControl = refreshControl
+        } else {
+            newsCollectionView.addSubview(refreshControl)
+        }
+        refreshControl.addTarget(self, action: #selector(refreshWeatherData(_:)), for: .valueChanged)
+        refreshControl.tintColor = .systemPink
+        refreshControl.attributedTitle = NSAttributedString(string: "Getting news...", attributes: nil)
+        
         // create UIPickerView
-               picker = UIPickerView(frame: CGRect(x: 0, y: self.view.bounds.height - 200, width: self.view.bounds.width, height: 200))
-        picker.backgroundColor = .blue
+        picker = UIPickerView(frame: CGRect(x: 0, y: self.view.bounds.height - 200, width: self.view.bounds.width, height: 200))
+        picker.backgroundColor = UIColor(displayP3Red: 191.0/255.0, green: 235.0/255.0, blue: 234.0/255.0, alpha: 1)
         picker.tintColor = .white
-               view.addSubview(picker)
-               
-               // create UIToolbar
-               toolbar = UIToolbar(frame: CGRect(x: 0, y: picker.frame.origin.y-40, width: self.view.frame.size.width, height: 40))
-               toolbar.barStyle = .black
-               toolbar.tintColor = .red
-               view.addSubview(toolbar)
-               let doneButtonItem = UIBarButtonItem(title: "done", style: .done, target: self, action: #selector(donePressed(sender:)))
-               
-               let cancelButtonItem = UIBarButtonItem(title: "cancel", style: .plain, target: self, action: #selector(cancelPressed(sender:)))
-               
-               toolbar.setItems([doneButtonItem, cancelButtonItem], animated: true)
-    
+        
+        view.addSubview(picker)
+        
+        // create UIToolbar with items
+        toolbar = UIToolbar(frame: CGRect(x: 0, y: picker.frame.origin.y-40, width: self.view.frame.size.width, height: 40))
+        toolbar.barStyle = .default
+        toolbar.tintColor = .systemPink
+        view.addSubview(toolbar)
+        
+        let doneButtonItem = UIBarButtonItem(title: "done", style: .done, target: self, action: #selector(donePressed(sender:)))
+        let cancelButtonItem = UIBarButtonItem(title: "cancel", style: .plain, target: self, action: #selector(cancelPressed(sender:)))
+        toolbar.setItems([doneButtonItem, cancelButtonItem], animated: true)
+        
         
         zeroRectTextField = UITextField(frame: CGRect.zero)
         zeroRectTextField.inputView = picker
         view.addSubview(zeroRectTextField)
         
-     
-       picker.isHidden = true
-       toolbar.isHidden = true
+        
+        picker.isHidden = true
+        toolbar.isHidden = true
         
         newsCollectionView.delegate = self
         newsCollectionView.dataSource = self
         picker.delegate = self
         picker.dataSource = self
     }
+   
+    @objc private func refreshWeatherData(_ sender: Any) {
+      news.getData{
+            DispatchQueue.main.async {
+                self.newsCollectionView.reloadData()
+            }
+        }
+        self.refreshControl.endRefreshing()
+    }
     
-    
-    func showTutorial(_ which: String) {
+    func showArticle(_ which: String) {
         if let url = URL(string: which) {
             let config = SFSafariViewController.Configuration()
             config.entersReaderIfAvailable = true
-
+            
             let vc = SFSafariViewController(url: url, configuration: config)
             present(vc, animated: true)
         }
     }
     
     @IBAction func chooseParams(_ sender: UIBarButtonItem) {
-      picker.isHidden = false
-      toolbar.isHidden = false
+        picker.isHidden = false
+        toolbar.isHidden = false
         picker.becomeFirstResponder()
-    }
-    
-    
-    
-    @objc func pickerValueChanged(sender: UIPickerView) {
-        
     }
     
     @objc func donePressed(sender: UIBarButtonItem) {
         
-       news.getData{
+        news.getData{
             DispatchQueue.main.async {
                 self.newsCollectionView.reloadData()
             }
@@ -111,13 +127,7 @@ class ViewController: UIViewController {
         picker.isHidden = true
         toolbar.isHidden = true
         picker.resignFirstResponder()
-
     }
-    
-    
-    
-    
-    
 } // the end of the class
 
 // MARK: - UICollectionView protocols
@@ -140,20 +150,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
         let articleImageURL = URL(string: articles?.urlToImage ?? "https://e3.365dm.com/20/11/768x432/skynews-brexit-breaking-news_5177180.jpg?20201123152327")!
         if let data = try? Data(contentsOf: articleImageURL) {
-             cell.newsImageView.image = UIImage(data: data)
+            cell.newsImageView.image = UIImage(data: data)
         }
-        
         return cell
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-      let articles = news.result?.articles[indexPath.row]
-       
+        let articles = news.result?.articles[indexPath.row]
+        
         let articleURL = articles?.url
-            self.showTutorial(articleURL!)
-        }
-
+        self.showArticle(articleURL!)
+    }
 }
 
 
@@ -168,11 +175,11 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         
         switch component {
             case 0:
-              return country.count
+                return country.count
             case 1:
-            return category.count
+                return category.count
             default:
-            return 1
+                return 1
         }
     }
     
@@ -180,13 +187,13 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         
         switch component {
             case 0:
-            return country[row]
+                return country[row]
             case 1:
-             return category[row]
+                return category[row]
             default:
-            return "ERROR: Can`t execute titleForRow func"
-       
-    }
+                return "ERROR: Can`t execute titleForRow func"
+            
+        }
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         switch component {
@@ -195,10 +202,7 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
             case 1:
                 news.category = category[row]
             default:
-            return
+                return
         }
-        
-       
     }
-    
 }
